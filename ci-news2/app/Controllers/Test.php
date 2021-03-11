04e60a06c904917187402e5d0475c7c9430618da
백인걸 <big9401@gmail.com>
2021-03-11 18:33:54 +0900

    try catch 문 작성

--- ci-news2/app/Controllers/Test.php
+++ ci-news2/app/Controllers/Test.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use CodeIgniter\Controller;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Exception;
 
 class Test extends Controller
 {
@@ -13,13 +14,21 @@ class Test extends Controller
     helper('date');
 }
   public function index() {
+    $sheet = null;
     $spreadSheet = new Spreadsheet();
     $sheet =$spreadSheet->getActiveSheet();
 
-    $sheetIndex = $spreadSheet->getIndex(
-      $spreadSheet->getSheetByName('Worksheet')
-    );
-    $spreadSheet->removeSheetByIndex($sheetIndex);
+    try {
+      $sheetIndex = $spreadSheet->getIndex( $spreadSheet->getSheetByName('1Worksheet') );
+    } catch (Exception $e) {
+      echo 'Message : '.$e->getMessage();
+    }
+
+    try {
+      $spreadSheet->removeSheetByIndex($sheetIndex);
+    } catch (Exception $e) {
+      echo 'Message: '.$e->getMessage();
+    }
 
     $sheet = $spreadSheet->createSheet();
     $sheet->setTitle("테스트 시트");
@@ -28,13 +37,13 @@ class Test extends Controller
     if($this->file_name == '') {
       $unix = now('Asia/Seoul'); // 현재시간 : UNIX 타임 스탬프
 
-      $this->file_name = "테스트파일_".date("Y_m_d", $unix);;
+      $this->file_name = "테스트파일_".date("Y_m_d", $unix);
     }
 
     header('Content-Type: application/vnd.ms-excel');
     header('Content-Disposition: attachment;filename="'. $this->file_name .'.xlsx"');
     header('Cache-Control: max-age=0');
     $writer->save('php://output');
-    return view("test/test");
+    echo view("test/test");
   }
 }
