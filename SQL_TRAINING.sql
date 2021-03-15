41f09f21d20ac153ff9aaa7a252b555833b16587
백인걸 <big9401@gmail.com>
2021-03-15 19:05:29 +0900

    기본 board, user 테이블 구조 수정

--- SQL_TRAINING.sql
+++ SQL_TRAINING.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE news;
 use news;
 
+/* v1.0 */
 CREATE TABLE board (
 	SNO INT(3) NOT NULL auto_increment primary key,
     SUBJECT_NAME CHAR(20) NOT NULL,
@@ -9,12 +10,24 @@ CREATE TABLE board (
     DATE_CHAR CHAR(20)
 );
 
-create table user (
-	id CHAR(10) not null primary key,
-	password CHAR(20),
-	name CHAR(10),
-	created_at timestamp not null default current_timestamp,
+/* v1.1 */
+CREATE TABLE board (
+	SNO INT(3) NOT NULL auto_increment primary key,
+    SUBJECT_NAME CHAR(20) NOT NULL,
+    CONTENT CHAR(200),
+    WRITER CHAR(20),
+    DATE_CHAR CHAR(20),
+    created_at timestamp not null default current_timestamp,
 	updated_at timestamp not null default current_timestamp on update current_timestamp
+);
+
+create table user (
+	SNO INT(3) not null auto_increment primary key,
+	ID CHAR(10) not null,
+	PASSWORD CHAR(20) not null,
+	NAME CHAR(10) not null,
+	CREATED_AT timestamp not null default current_timestamp,
+	UPDATED_AT timestamp not null default current_timestamp on update current_timestamp
 );
 
 SET SQL_SAFE_UPDATES = 0; # safe mode OFF.
@@ -22,8 +35,13 @@ DELETE FROM board;
 DELETE from user;
 SET SQL_SAFE_UPDATES = 1; # safe mode ON.
 
+
+/* create admin id */
+insert into news.user(id, password, name) values ('admin', '12345', 'admin');
+
 select * from board;
 select * from user;
 
+drop table board;
 drop table user;
 
