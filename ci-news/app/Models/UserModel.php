4bb8ac2c89ae2b3ed99c92ec313966a16ab75026
백인걸 <big9401@gmail.com>
2021-04-30 10:49:34 +0900

    UserModel 수정

--- ci-news/app/Models/UserModel.php
+++ ci-news/app/Models/UserModel.php
@@ -1,31 +1,12 @@
 <?php namespace App\Models;
 
-use CodeIgniter\Database\BaseConnection;
 use CodeIgniter\Model;
 
 class UserModel extends Model {
-  protected $db;
+  protected string $table = "users";
+  protected string $primaryKey = "SNO";
 
-  protected $table = "users";
-  protected $allowedFields = ['SNO', 'SUBJECT_NAME', 'COTNENT', 'WRITER', 'DATE_CHAR'];
+  protected array $allowedFields = ['SNO', 'ID', 'PWD', 'NAME', 'MAIL', 'CREATED_AT', 'UPDATED_AT'];
+  protected string $returnType =  "string";
 
-  protected $returnType =  'string';
-
-  protected $primaryKey = 'SNO';
-
-  public function __construct() {
-    $this->db = db_connect();
-    $this->db->table('user');
-    $query = $this->db->query('SELECT * FROM users');
-    $result = $query->getResult();
-    $this->db->close();
-  }
-
-  public function getUsers($slug = false): array
-  {
-    if($slug === false) {
-      return $this->findAll();
-    }
-
-  }
 }
