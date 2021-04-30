a5a6810900f9ae356e01e8f2410f5acebf3e0b29
백인걸 <big9401@gmail.com>
2021-04-30 10:43:33 +0900

    게시판 모델 주석 추가

--- ci-news/app/Models/BoardModel.php
+++ ci-news/app/Models/BoardModel.php
@@ -3,12 +3,14 @@ namespace App\Models;
 
 use CodeIgniter\Model as Model;
 
+/* @author GEOL <big9401@gmail.com>
+ * @see BoardModel 게시판 데이터베이스 정의
+ */
 class BoardModel extends Model {
   protected string $table = "board";
   protected string $primaryKey= "SNO";
 
   protected bool $useAutoIncrement = true;
-//  protected string $returnType = 'array';
 
   protected array $allowedFields = ['SNO', 'SUBJECT_NAME', 'CONTENT', 'WRITER', 'DATE_CHAR', 'HIT', 'created_at', 'updated_at'];
   protected array $validationRules    = [];
