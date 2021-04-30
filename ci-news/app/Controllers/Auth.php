7a8228f542a92231259b5dbf3de550da9758938e
백인걸 <big9401@gmail.com>
2021-04-30 15:32:51 +0900

    session check, create, destroy

--- ci-news/app/Controllers/Auth.php
+++ ci-news/app/Controllers/Auth.php
@@ -13,12 +13,56 @@ class Auth extends Controller {
 
   }
 
-  public function login() {
-    echo 'Hello Auth login()';
-//    $session = Services::session($config);
-    $session = session();
+  public function createSession() {
+    $session = Services::session();
+    $mySession = array(
+      'name' => 'geol',
+      'city' => 'seoul'
+    );
+    $session->set($mySession);
 
-    var_dump($session);
-    echo '</pre>';
+//    echo '<pre>';
+//    var_dump($session);
+//    echo '</pre>';
+  }
+
+  public function checkSession() {
+    $session = Services::session();
+    if( $session->has('name') ) {
+      echo $session->get('name');
+      echo '</br>';
+      echo 'session exist';
+    } else {
+      echo 'session not exist';
+    }
+  }
+
+  public function destroySession() {
+    $session = Services::session();
+    if( $session->has('name') ) {
+      $session->destroy();
+      echo '</br>';
+      echo 'session destroy';
+    } else {
+      echo 'error here';
+    }
+  }
+
+  public function login() {
+//    $this->createSession();
+//    $this->response->redirect('/');
+
+    echo view('header');
+    echo view('auth/login');
+  }
+
+  public function logout() {
+    $this->destroySession();
+    $this->response->redirect('/');
+  }
+
+  public function loginProc() {
+
+    $this->createSession();
   }
 }
