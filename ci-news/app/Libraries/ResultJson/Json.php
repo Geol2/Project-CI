56fa4b946134320bf3eeb849d36bface9a1e58d2
백인걸 <big9401@gmail.com>
2021-05-11 15:27:05 +0900

    커스텀 JSON 인터페이스 및 클래스 작성

---
+++ ci-news/app/Libraries/ResultJson/Json.php
@@ -0,0 +1,42 @@
+<?php namespace App\Libraries\ResultJson;
+
+/* @author GEOL <big9401@gmail.com>
+ * @see json 인터페이스 정의
+ */
+interface JsonInterface {
+  public function success();
+  public function fail();
+}
+
+/* @author GEOL <big9401@gmail.com>
+ * @see json 추상 클래스 정의
+ */
+abstract class JsonAbstract implements JsonInterface
+{
+  abstract public function success();
+  abstract public function fail();
+}
+
+/* @author GEOL <big9401@gmail.com>
+ * @see json 클래스 정의
+ */
+class Json extends JsonAbstract {
+
+  public function success($data = null) {
+    // TODO: Implement success() method.
+    $result = array(
+      'code' => 200,
+      'data' => 'success'
+    );
+    return json_encode($result, JSON_UNESCAPED_UNICODE);
+  }
+
+  public function fail($data = null) {
+    // TODO: Implement fail() method.
+    $result = array(
+      'code' => 400,
+      'data' => 'fail'
+    );
+    return json_encode($result, JSON_UNESCAPED_UNICODE);
+  }
+}
